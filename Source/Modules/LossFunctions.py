e914edd3c4819c169db90b531254e5ede3116512
Bob Giezi <bob.giezi@gmail.com>
2024-11-21 12:54:03 -0500

    update loss function

--- Source/Modules/LossFunctions.py
+++ Source/Modules/LossFunctions.py
@@ -5,7 +5,7 @@ def wind_loss(output, target):
     u_error = output[..., 0] - target[..., 0]
     v_error = output[..., 1] - target[..., 1]
 
-    error = torch.sqrt((u_error**2) + (v_error**2))
+    error = torch.sqrt((u_error**2) + (v_error**2) + torch.finfo(torch.float32).eps)
 
     loss = torch.mean(error)
 
